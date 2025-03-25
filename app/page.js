03506cd6f293a45391bba7f1c3a564c52bdbde59
AnmolDeshmukh <anmoldeshmukh101@gmail.com>
2025-03-25 12:11:09 -0700

    Fix: Re-enabled Blog component after moving fetch logic to client-side

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react';
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
-// import Blog from "./components/homepage/blog";
+import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
@@ -44,7 +44,7 @@ export default function Home() {
       <Skills />
       <Projects />
       <Education />
-      {/* <Blog blogs={blogs} /> */}
+      <Blog blogs={blogs} />
       <ContactSection />
     </div>
   );
