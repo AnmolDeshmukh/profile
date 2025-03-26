6fc6b82af8fa4882fb953ed255a6f2fbe369e08a
AnmolDeshmukh <anmoldeshmukh101@gmail.com>
2025-03-26 12:36:59 -0700

    Test: Add ContactSection

--- app/page.js
+++ app/page.js
@@ -52,11 +52,13 @@
 'use client'
 import HeroSection from "./components/homepage/hero-section";
 import AboutSection from "./components/homepage/about";
+import ContactSection from "./components/homepage/contact";
 export default function Home() {
   return (
     <div suppressHydrationWarning>
       <HeroSection />
       <AboutSection />
+      <ContactSection />
     </div>
   );
 }
